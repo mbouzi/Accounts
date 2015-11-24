869297468119fef995045211ae9bdd8b8fad6bac
mbouzi <mrbouzi@gmail.com>
2015-11-23 22:55:48 -0500

    create record component

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
+  root :to => 'records#index'
   resources :records
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
